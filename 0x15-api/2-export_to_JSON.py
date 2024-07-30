802f03747208ad02979fbdb471f1fb4ba4f5e5ac
patmuchiri <pmuchirig@gmail.com>
2024-07-30 16:43:00 +0300

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-""" Python to get data from an API and convert it to Json"""
-import csv
+
+""" Export to the JSON file """
+
+from requests import get
 import json
-import requests
 import sys
 
 
 if __name__ == '__main__':
-    USER_ID = sys.argv[1]
-    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
-    res = requests.get(url_to_user)
-    """Documentation"""
-    USERNAME = res.json().get('username')
-    """Documentation"""
-    url_to_task = url_to_user + '/todos'
-    res = requests.get(url_to_task)
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    res = get(url)
+    username = res.json().get('username')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    res = get(url)
     tasks = res.json()
 
-    dict_data = {USER_ID: []}
+    data = {user_id: []}
+
     for task in tasks:
-        TASK_COMPLETED_STATUS = task.get('completed')
-        TASK_TITLE = task.get('title')
-        dict_data[USER_ID].append({
-                                  "task": TASK_TITLE,
-                                  "completed": TASK_COMPLETED_STATUS,
-                                  "username": USERNAME})
-    """print(dict_data)"""
-    with open('{}.json'.format(USER_ID), 'w') as f:
-        json.dump(dict_data, f)
+        data[user_id].append({
+                              "task": task.get('title'),
+                              "completed": task.get('completed'),
+                              "username": username
+                              })
+
+    with open('{}.json'.format(user_id), 'w') as file:
+        json.dump(data, file)
