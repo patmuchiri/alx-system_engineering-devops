8309b9e5515ebcf069f843d53933af535d686d94
patmuchiri <pmuchirig@gmail.com>
2024-08-21 12:20:53 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,12 @@ Script queries subscribers on a given Reddit subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return 'OK' if subreddit exists, otherwise 'OK'."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
