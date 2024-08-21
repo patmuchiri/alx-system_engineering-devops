879d2c02440902cbcc9c2a5f06bbd5b8773ffa5f
patmuchiri <pmuchirig@gmail.com>
2024-08-21 12:11:59 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """
-this script queries subscibers on a given Reddit subreddit.
+Script queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise HTTPError for bad responses
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
-    except requests.RequestException:
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
